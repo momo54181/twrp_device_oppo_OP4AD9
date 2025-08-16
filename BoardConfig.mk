ce6ecb2d38f3466cbf921e6506cb35351f04d0e3
Momo5418 <momo5418@mocn.top>
2025-08-16 15:32:33 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,9 +110,6 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_NO_SCREEN_BLANK := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TARGET_USES_MKE2FS := true
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/backlight/panel0-backlight/brightness"
-TW_DEFAULT_BRIGHTNESS := 1620
-TW_MAX_BRIGHTNESS := 2047
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
