cb37126e9b1c42afda43a1c3c43786afa5c0e812
Momo5418 <momo5418@mocn.top>
2025-08-16 13:28:50 +0800

    Update

--- twrp_OP4AD9.mk
+++ twrp_OP4AD9.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/oppo/OP4AD9/device.mk)
 
 PRODUCT_DEVICE := OP4AD9
-PRODUCT_NAME := omni_OP4AD9
+PRODUCT_NAME := twrp_OP4AD9
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := OPPO Ace2
 PRODUCT_MANUFACTURER := oppo
@@ -24,10 +24,6 @@ PRODUCT_MANUFACTURER := oppo
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Extra required packages
-PRODUCT_PACKAGES += \
-    libion
-    
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
@@ -42,4 +38,4 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1
+    android.hardware.keymaster@4.0
